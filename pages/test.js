da5815924ce47e3f5661a4f7d5002a8d78c36492
sergey <dimovitch.sergey@gmail.com>
2020-11-24 12:15:37 +0300

    init commit

---
+++ pages/test.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const API_BASE = "http://localhost:5000";
+
+function Test(res) {
+	console.log(res)
+	return (
+		<div>
+			
+			<h1></h1>
+		)
+		</div>
+
+	)
+}
+
+
+
+
+export default Test;
